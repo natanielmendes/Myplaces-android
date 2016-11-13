9dc8e365058910e5c47872d4c004220091d6e856
nataniel <natanielmendes@hotmail.com>
2016-11-13 19:16:52 -0200

    save dinamically title of places

---
+++ myplaces-android/src/main/java/com/placestudio/myplaces_android/EditActivity.java
@@ -0,0 +1,38 @@
+package com.placestudio.myplaces_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class EditActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        final LatLng latlng = (LatLng) getIntent().getParcelableExtra("location");
+
+        final EditText title = (EditText) findViewById(R.id.title);
+        Button boton = (Button) findViewById(R.id.save);
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                MarkerOptions marker = new MarkerOptions().position(latlng);
+                if (title.getText() != null) {
+                    marker.title(title.getText().toString());
+                }
+
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("marker", marker);
+                setResult(Activity.RESULT_OK, resultIntent);
+                finish();
+            }
+        });
+    }
+}
